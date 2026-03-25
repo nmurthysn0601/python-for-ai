d6c1a75b2539fb44a79944bc87644a88a5866ea1
nmurthysn0601 <narasimhamurthy.sn06@gmail.com>
2026-03-25 16:06:12 +0530

    added hello print in hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,13 @@
 class Dog:
     def __init__(self, name):
         self.name = name
-    
+
     def bark(self):
         print("Bark!!!")
-        
+
+
 jerry = Dog(name="Jerry")
 
 print(jerry.name)
 print(jerry.bark())
+print("hello")
